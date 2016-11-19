59b18faaf566462b4312572c0bd9675da3c252a0
zeroway <M_O_Bz@163.com>
2016-11-19 11:27:57 +0000

    add 3288 android 4.4

--- gen_update.sh
+++ gen_update.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-set -x
-
 # android top directory
+ANDROID_TOP_DIR_3288_4="/home/zeroway/3288/src/3288_4.4"
 ANDROID_TOP_DIR_3288="/home/zeroway/3288/51/src/3288_5.1_v2"
 ANDROID_TOP_DIR_3368="/home/zeroway/3368/src/3368"
 
@@ -27,7 +26,7 @@ CURRENT_DATE=`date +%Y%m%d`
 pack_images()
 {
 	$UPDATES_GEN_DIR/afptool -pack $UPDATES_GEN_DIR/ $UPDATES_GEN_DIR/Image/update.img || pause
-	$UPDATES_GEN_DIR/rkImageMaker $PLATFORM $TARGET_BOOTLOADER_IMG $UPDATES_GEN_DIR/Image/update.img "update_${SOC_NAME}_${CURRENT_DATE}_linux.img" -os_type:androidos || pause
+	$UPDATES_GEN_DIR/rkImageMaker $PLATFORM $TARGET_BOOTLOADER_IMG $UPDATES_GEN_DIR/Image/update.img "update_${SOC_NAME}_${ANDROID_VERSION}_${CURRENT_DATE}_linux.img" -os_type:androidos || pause
 }
 
 # make links
@@ -58,6 +57,38 @@ make_links()
 	ln -s $TARGET_PARAMETER_FILE $PARAMETER
 }
 
+make_update_3288_4()
+{
+	# 3288 android 4.4 target images
+	TARGET_KERNEL_IMG="$ANDROID_TOP_DIR_3288_4/out/target/product/rk3288/obj/KERNEL/kernel.img"
+	TARGET_RESOURCE_IMG="$ANDROID_TOP_DIR_3288_4/out/target/product/rk3288/obj/KERNEL/resource.img"
+	TARGET_RECOVERY_IMG="$ANDROID_TOP_DIR_3288_4/rockdev/Image-rk3288/recovery.img"
+	TARGET_SYSTEM_IMG="$ANDROID_TOP_DIR_3288_4/rockdev/Image-rk3288/system.img"
+	TARGET_BOOT_IMG="$ANDROID_TOP_DIR_3288_4/rockdev/Image-rk3288/boot.img"
+	TARGET_MISC_IMG="$ANDROID_TOP_DIR_3288_4/rockdev/Image-rk3288/misc.img"
+	target_imgs=(
+		$TARGET_KERNEL_IMG
+		$TARGET_RESOURCE_IMG
+		$TARGET_RECOVERY_IMG
+		$TARGET_SYSTEM_IMG
+		$TARGET_BOOT_IMG
+		$TARGET_MISC_IMG
+	)
+
+	TARGET_PACKAGE_FILE="$UPDATES_GEN_DIR/package-file_3288"
+	TARGET_PARAMETER_FILE="$UPDATES_GEN_DIR/parameter_3288_4"
+	TARGET_BOOTLOADER_IMG="$UPDATES_GEN_DIR/RK3288UbootLoader_V2.19.10.bin"
+
+	# make links for the metadata
+	make_links
+
+	echo "make 3288 4.4 update.img"
+	PLATFORM="-RK32"
+	SOC_NAME="3288"
+	ANDROID_VERSION="4.4"
+	pack_images
+}
+
 make_update_3288()
 {
 	# 3288 target images
@@ -86,6 +117,7 @@ make_update_3288()
 	echo "make 3288 5.1 update.img"
 	PLATFORM="-RK32"
 	SOC_NAME="3288"
+	ANDROID_VERSION="5.1"
 	pack_images
 }
 
@@ -121,20 +153,27 @@ make_update_3368()
 	echo "make 3368 5.1 update.img"
 	PLATFORM="-RK330A"
 	SOC_NAME="3368"
+	ANDROID_VERSION="5.1"
 	pack_images
 }
 
 print_usage()
 {
 	echo "Usage: genupdate <soc_name>"
-	echo "genupdate 3288"
+	echo "genupdate 32885 ==> 3288 5.1"
+	echo "genupdate 32884 ==> 3288 5.1"
+	echo "genupdate 32884"
 	echo "genupdate 3368"
 }
 
-if [ "$1" = "3288" ]
+if [ "$1" = "32885" ]
 then
 	# make 3288 5.1 update.img
 	make_update_3288
+elif [ "$1" = "32884" ]
+then
+	# make 3288 4.4 update.img
+	make_update_3288_4
 elif [ "$1" = "3368" ]
 then
 	# make 3368 5.1 update.img
